9490bd9535c37caf38968029d194c936a90f1ed9
jerry <brownjer3@gmail.com>
2020-10-20 13:50:16 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(" and ")
+    else
+        new_arr =[]
+        array.each_with_index do |item, index|
+            if index < array.size - 2
+                new_item = item << ", "
+                new_arr.push(new_item)
+            elsif index == array.size - 2
+                new_item = item << ", and "
+                new_arr.push(new_item)
+            else
+                new_arr.push(item)
+            end
+        end
+        new_arr.join
+    end
 end
+
+
